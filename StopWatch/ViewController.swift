22c8910f58248d80709e31000824e921e514fe95
Yoorian Shin <yoorian.shin@gmail.com>
2016-08-30 21:11:19 -0700

    Initial Commit

---
+++ StopWatch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StopWatch
+//
+//  Created by Yoorian YeEun Shin on 8/30/16.
+//  Copyright © 2016 Yoorian Shin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
